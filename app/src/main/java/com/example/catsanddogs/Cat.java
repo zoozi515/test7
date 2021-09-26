d03c2aaf673df18b9d48d51afb2d6b6ba7f66ebe
Zahra Yousef <45607455+zoozi515@users.noreply.github.com>
2021-09-26 16:31:19 +0300

    ver 6

---
+++ app/src/main/java/com/example/catsanddogs/Cat.java
@@ -0,0 +1,4 @@
+package com.example.catsanddogs;
+
+public class Cat {
+}
---
+++ app/src/main/java/com/example/catsanddogs/Dog.java
@@ -0,0 +1,11 @@
+package com.example.catsanddogs;
+
+public class Dog {
+    String name;
+    int age;
+
+    public void park(String name){
+        System.out.println(name + "woof");
+
+    }
+}
